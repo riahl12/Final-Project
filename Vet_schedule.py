32227b0141f4285a5b4f99689e8f3e1c840a48b4
riahl12 <riley.ahlrichs@gmail.com>
2022-12-08 17:19:22 -0600

    Updated code

--- Vet_schedule.py
+++ Vet_schedule.py
@@ -1,40 +1,49 @@
-class PatientInfo:
+import heapq
+
+class PatientInfo: #This class takes in attributes for the owner first and last name, pet name, species and priority(meaning what order will the vet see them in)
     def __init__(self, o_fname, o_lname, petname, species, priority):
-        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-") #this is setting that only string characters can be entered to validate input
+                                                                                        #for the schedule
         if not (name_characters.issuperset(o_fname) and name_characters.issuperset(o_lname) 
             and name_characters.issuperset(petname) and name_characters.issuperset(species)):
-            raise ValueError
-        self.o_fname = o_fname
-        self.o_lname = o_lname
-        self.petname = petname
-        self.species = species
-        self.priority = priority
+            raise ValueError("The inputs must be a letter.")
+        else:
+            self.o_fname = o_fname
+            self.o_lname = o_lname
+            self.petname = petname
+            self.species = species
+            self.priority = priority
 
-class Schedule:
+class Schedule: #The class initializes an empty list to save the information gathered from the PetInfo class
     def __init__(self):
-        self.queue = []
+        self.pqueue = []
 
-    def insert(self, node): 
+    def insert(self, node): #This function inserts the PetInfo into the empty list and will sort based on the priority
+                            #it will put the appointments in order from first to last
         if(self.size() == 0):
-            self.queue.append(node)
+            self.pqueue.append(node)
         else:
             for x in range(0, self.size()):
-                if(node.priority >= self.queue[x].priority):
+                if(node.priority >= self.pqueue[x].priority):
                     if(x == (self.size() - 1)):
-                        self.queue.insert(x + 1, node)
+                        self.pqueue.insert(x + 1, node)
                     else:
                         continue
                 else:
-                    self.queue.insert(x, node)
+                    self.pqueue.insert(x, node)
 
-    def delete(self): #have it so that it takes off the beginning node first meaning it is taking off the highest priority first
-        return self.queue.pop(0)
+
+    def delete(self): #This function will take off the highest priority appointment first since the highest
+                        #priority will always be at index 0
+        return self.pqueue.pop(0)
     
-    def size(self):
-        return len(self.queue)
+    def size(self): #This function will return the length of the daily schedule, the vet can see how many appointments they have 
+                    #in a given day
+        return len(self.pqueue)
     
-    def print(self):
-        for x in self.queue:
+    def print(self):#This function will print out the information for the schedule along with the priority next to it
+                    #so the vet can make sure they are seeing everyone in the correct order
+        for x in self.pqueue:
             print("Owner name: {} {}, Pet Name: {}, Species: {} - {}".format(x.o_fname, x.o_lname, x.petname, x.species, x.priority))
     
 
@@ -45,6 +54,7 @@ if __name__ == "__main__":
     pet5 = PatientInfo("bob","dylan","sparky","cat","5")
     pet3 = PatientInfo("Holly", "Johnson", "Kenny", "Other","3")
     pet4 = PatientInfo("Sam", "Jackson", "Percy", "Dog","4")
+    pet6 = PatientInfo("Hailey","Jones","sally","Cat","6")
    
 
     schedule1.insert(pet3)
@@ -52,7 +62,12 @@ if __name__ == "__main__":
     schedule1.insert(pet2)
     schedule1.insert(pet5)
     schedule1.insert(pet4)
+    schedule1.insert(pet6)
     schedule1.print()
+    print("Today's schedule has:",schedule1.size()," patients.")
 
+    
+    print("\nSchedule after 2 patients have been seen:")
+    schedule1.delete()
     schedule1.delete()
     schedule1.print()
--- Vet_schedule_gui.py
+++ Vet_schedule_gui.py
@@ -1,84 +1,93 @@
 from distutils import command
-import tkinter
+from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from csv import *
 
 
-def enter_data():
-    accepted = accept_var.get()
-
-    if accepted=="Accepted":
-
-    #User info
-        firstname = first_name_entry.get()
-        lastname = last_name_entry.get()
-
-        if firstname and lastname:
-            title = title_combobox.get()
-            phone = phone_label_entry.get()
-            email = email_label_entry.get()
-            email_opt = email_opt_combobox.get()
-
-        #Pet info
-            pet_name = petname_entry.get()
-            pet_species = pet_species_combobox.get()
-
-            if pet_name and pet_species:
-                pet_age = petage_spinbox.get()
-                pet_gender = pet_gender_combobox.get()
-                dog_breed = dogbreed_combobox.get()
-                cat_breed = catbreed_combobox.get()
-                
-
-                print("Title:", title,"First name:", firstname, "Last name:", lastname)
-                print("Phone #:", phone, "Email:", email, "Email Opt-In Service:", email_opt)
-                print("Pet Name:", pet_name, "Species:", pet_species,"Pet Age:", pet_age)
-                print("Gender:", pet_gender, "Dog Breed:", dog_breed, "Cat Breed:", cat_breed)
-                print("----------------------------------------------------------")
-            else:
-                tkinter.messagebox.showwarning(title = "Error", message = "Pet name and species are required to sumbit.")
-        else:
-            tkinter.messagebox.showwarning(title = "Error", message = "First name and last name are required to submit.")
-    else:
-        tkinter.messagebox.showwarning(title = "Error", message="You have not entered all the required information.")
+main_list = []
+header = ["Title ", " First name ", " Last name " , " Phone # " , " Email " , " Email Opt-In Service " ,
+                             " Pet Name " , " Species " , " Pet Age " , " Gender " , " Dog Breed " , " Cat Breed"]
 
 
-window = tkinter.Tk()
+def add():  #checks that the correct input is allowed for the different required fields, then grabs all the entries and appends them into the main list
+    name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-") #this is setting that only string characters can be entered to validate input
+                                                                                        #for the schedule
+    if not (name_characters.issuperset(first_name_entry.get()) and name_characters.issuperset(last_name_entry.get()) 
+        and name_characters.issuperset(petname_entry.get())):
+        messagebox.showerror("Error","The first name, last name and pet name must be letters. Try again.")
+
+    phone_characters = set("123456789-")
+    if not (phone_characters.issuperset(phone_label_entry.get())):
+        messagebox.showerror("Error","That is not a phone number. Try again.")
+
+    lst = [title_combobox.get() + ' ', first_name_entry.get() + ' ', last_name_entry.get() +' ', phone_label_entry.get() + ' ' ,
+            email_label_entry.get() +' ', email_opt_combobox.get() +' ', petname_entry.get() +' ', pet_species_combobox.get() +' ' ,
+             petage_spinbox.get() +' ', pet_gender_combobox.get() +' ', dogbreed_combobox.get() +' ', catbreed_combobox.get()]
+    main_list.append(lst)
+    messagebox.showinfo("Check-In", "You are checking in.")
+
+def save():#This function is what creates the csv file and combines the header that was created at the top along with writing the main list that
+            #holds all the entries, then when the button is clicked it will tell the user the information was saved successfully
+    with open("vet_schedule.csv", "w") as file:
+        Writer = writer(file, delimiter=',')
+        Writer.writerow(header)
+        Writer.writerows(main_list)
+        messagebox.showinfo("Check-In", "Your information was saved successfully.")
+
+def clear():#This function is used if you need to add multiple entries at the same time and will clear all the
+            #fields for you to be able to continue to use the program
+    title_combobox.delete(0,END)
+    first_name_entry.delete(0,END)
+    last_name_entry.delete(0,END)
+    phone_label_entry.delete(0,END)
+    email_label_entry.delete(0,END)
+    email_opt_combobox.delete(0,END)
+    petname_entry.delete(0,END)
+    pet_species_combobox.delete(0,END)
+    petage_spinbox.delete(0,END)
+    pet_gender_combobox.delete(0,END)
+    dogbreed_combobox.delete(0,END)
+    catbreed_combobox.delete(0,END)
+
+
+
+window = Tk()   #The start of the GUI creation
 window.title("Vet Schedule")
 
-frame = tkinter.Frame(window)
+frame = Frame(window)
 frame.pack()
 
 #User Info
-user_info_frame = tkinter.LabelFrame(frame, text="Owner Information")
+user_info_frame = LabelFrame(frame, text="Owner Information")
 user_info_frame.grid(row= 0, column=0, padx=25, pady=10)
 
-first_name_label = tkinter.Label(user_info_frame, text="First Name")
+first_name_label = Label(user_info_frame, text="First Name")
 first_name_label.grid(row=0, column=0)
-last_name_label = tkinter.Label(user_info_frame, text="Last Name")
+last_name_label = Label(user_info_frame, text="Last Name")
 last_name_label.grid(row=0, column=1)
 
-first_name_entry = tkinter.Entry(user_info_frame)
-last_name_entry = tkinter.Entry(user_info_frame)
+first_name_entry = Entry(user_info_frame)
+last_name_entry = Entry(user_info_frame)
 first_name_entry.grid(row=1, column=0)
 last_name_entry.grid(row=1, column=1)
 
-title_label = tkinter.Label(user_info_frame, text="Title")
-title_combobox = ttk.Combobox(user_info_frame, values=["","Mr.","Ms.","Mrs."])
+title_label = Label(user_info_frame, text="Title")
+title_combobox = ttk.Combobox(user_info_frame, values=["","Dr.","Mr.","Ms.","Mrs."])
 title_label.grid(row=0, column=2)
 title_combobox.grid(row=1, column=2)
 
-phone_label = tkinter.Label(user_info_frame, text="Phone Number")
+phone_label = Label(user_info_frame, text="Phone Number")
 phone_label.grid(row=2, column=0)
-phone_label_entry = tkinter.Entry(user_info_frame)
+phone_label_entry = Entry(user_info_frame)
 phone_label_entry.grid(row=3, column=0)
 
-email_label = tkinter.Label(user_info_frame, text="Email")
+email_label = Label(user_info_frame, text="Email")
 email_label.grid(row=2, column=1)
-email_label_entry = tkinter.Entry(user_info_frame)
+email_label_entry = Entry(user_info_frame)
 email_label_entry.grid(row=3, column=1)
 
-email_opt_label = tkinter.Label(user_info_frame, text="Email Opt-In Service")
+email_opt_label = Label(user_info_frame, text="Email Opt-In Service")
 email_opt_combobox = ttk.Combobox(user_info_frame, values=["Yes", "No", "Not now"])
 email_opt_label.grid(row=2, column=2)
 email_opt_combobox.grid(row=3, column=2)
@@ -87,37 +96,37 @@ for widget in user_info_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
 #Pet info
-pet_info_frame = tkinter.LabelFrame(frame, text="Pet Information")
+pet_info_frame = LabelFrame(frame, text="Pet Information")
 pet_info_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
 
-petname_label = tkinter.Label(pet_info_frame, text="Pet Name")
-petname_entry = tkinter.Entry(pet_info_frame)
+petname_label = Label(pet_info_frame, text="Pet Name")
+petname_entry = Entry(pet_info_frame)
 petname_label.grid(row=0, column=0)
 petname_entry.grid(row=1, column=0)
 
-pet_species_label = tkinter.Label(pet_info_frame, text="Species")
+pet_species_label = Label(pet_info_frame, text="Species")
 pet_species_combobox = ttk.Combobox(pet_info_frame, values=["","Dog", "Cat","Other"])
 pet_species_label.grid(row=0, column=1)
 pet_species_combobox.grid(row=1, column=1)
 
-petage_label = tkinter.Label(pet_info_frame, text="Pet Age")
-petage_spinbox = tkinter.Spinbox(pet_info_frame, from_=0, to=20)
+petage_label = Label(pet_info_frame, text="Pet Age")
+petage_spinbox = Spinbox(pet_info_frame, from_=0, to=20)
 petage_label.grid(row=0, column=2)
 petage_spinbox.grid(row=1, column=2)
 
-pet_gender_label = tkinter.Label(pet_info_frame, text="Gender")
+pet_gender_label = Label(pet_info_frame, text="Gender")
 pet_gender_combobox = ttk.Combobox(pet_info_frame, values=["Male", "Female", "Neutered Male", "Spayed Female"])
 pet_gender_label.grid(row=2, column=0)
 pet_gender_combobox.grid(row=3, column=0)
 
 
-dogbreed_label = tkinter.Label(pet_info_frame, text="Dog Breeds")
+dogbreed_label = Label(pet_info_frame, text="Dog Breeds")
 dogbreed_combobox = ttk.Combobox(pet_info_frame, values=["Not Dog","Bulldog", "Beagle", "French Bulldog", "German Shepherd","Golden Retriever","Australian Shepherd",
                                                         "Corgi","Goldendoodle","Mixed Breed","Labrador Retriever","Dachshund", "Great Dane","Husky"])
 dogbreed_label.grid(row=2, column=1)
 dogbreed_combobox.grid(row=3,column=1)
 
-catbreed_label = tkinter.Label(pet_info_frame, text="Cat Breeds")
+catbreed_label = Label(pet_info_frame, text="Cat Breeds")
 catbreed_combobox = ttk.Combobox(pet_info_frame, values=["Not Cat","Domestic Shorthair", "Domestic Mediumhair", "Domestic Longhair", "Scottish Fold","Maine Coon",
                                                          "Persain","Ragdoll", "Siamese","Bengal","Manx","Munchkin","Russian Blue", "Sphynx"])
 catbreed_label.grid(row=2, column=2)
@@ -126,19 +135,21 @@ catbreed_combobox.grid(row=3, column=2)
 for widget in pet_info_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
-#Accept terms
-terms_frame = tkinter.LabelFrame(frame, text="Verify Information")
-terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+#Button creation
+add_button = Button(frame, text="Add Info", command=add)
+add_button.grid(row=4,column=0,sticky="news",padx=20, pady=10)
 
-accept_var = tkinter.StringVar(value="Not Accepted")
-terms_check = tkinter.Checkbutton(terms_frame, text="I have entered the correct information.",
-                                        variable=accept_var, onvalue="Accepted", offvalue="Not Accepted")
-terms_check.grid(row=0, column=0)
+save_button = Button(frame, text="Save Info", command=save)
+save_button.grid(row=5, column=0,sticky="news", padx=20, pady=10)
 
-#Button
-button = tkinter.Button(frame, text="Check-In", command= enter_data)
-button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+clear_button = Button(frame, text="Clear", command= clear)
+clear_button.grid(row=6, column=0, sticky="news", padx=20, pady=10)
+
+
 
 
 
 window.mainloop()
+
+
+
